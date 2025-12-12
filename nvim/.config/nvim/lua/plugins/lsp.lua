9dae96e875f1b5bf256f6277c08f72e979557985
adursh <education@erased.aleeas.com>
2025-12-13 01:57:49 +0530

    optimised config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -44,7 +44,7 @@ return {
 				callback = function(args)
 					local opts = { buffer = args.buf }
 					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					-- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, opts)
 					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
