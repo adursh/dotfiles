f72a9c1053b7696edfbabcd9c9bfaac64f80ecb6
Novice <nvc@Penguin.local>
2026-03-23 05:02:58 +0530

    fixed file structure

--- kanata/kanata_start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-KANATA_BIN="$HOME/.config/kanata/executables/kanata_macos_cmd_allowed_arm64"
-KANATA_CFG="$HOME/.config/kanata/callum.kbd"
-exec sudo "$KANATA_BIN" --cfg "$KANATA_CFG"
