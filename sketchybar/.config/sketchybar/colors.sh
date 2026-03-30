a793354a982ead8b0d89a8ba217cddcacc0239bf
Novice <nvc@Penguin.local>
2026-03-31 03:26:09 +0530

    +apple logo, bar color change

--- sketchybar/.config/sketchybar/colors.sh
+++ sketchybar/.config/sketchybar/colors.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export BAR_COLOR=0xff11111b
+# export BAR_COLOR=0xff11111b
+# export BAR_COLOR=0xff1e1e2e
+export BAR_COLOR=0xff181825
 export SURFACE0=0xff313244
 export SURFACE1=0xff45475a
 export SUBTEXT0=0xff7f849c
--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -6,13 +6,13 @@ TARGET=$(printf "1\n%s\n%s\n" "$OCCUPIED" "$FOCUSED_WORKSPACE" | sort -un)
 for sid in 1 2 3 4 5 6 7 8 9 10; do
   if echo "$TARGET" | grep -qw "$sid"; then
     if [ "$sid" = "$FOCUSED_WORKSPACE" ]; then
-      sketchybar --animate tanh 15 --set "space.$sid" \
+      sketchybar --animate tanh 10 --set "space.$sid" \
         drawing=on \
         label.color=0xffffffff \
-        label.font="SF Pro Text:Bold:15.0" \
+        label.font="SF Pro Text:Semibold:14.0" \
         background.drawing=off
     else
-      sketchybar --animate tanh 15 --set "space.$sid" \
+      sketchybar --animate tanh 10 --set "space.$sid" \
         drawing=on \
         label.color=0xff3b3b4f \
         label.font="SF Pro Text:Medium:14.0" \
