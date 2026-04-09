202742a6becf8b677308bd33fc039cb5e9c68809
Novice <nvc@Penguin.local>
2026-04-09 21:00:03 +0530

    remove callum

--- kanata/.config/kanata/kanata_start.sh
+++ kanata/.config/kanata/kanata_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 KANATA_BIN="$HOME/.config/kanata/executables/kanata_macos_cmd_allowed_arm64"
-KANATA_CFG="$HOME/.config/kanata/callum.kbd"
+KANATA_CFG="$HOME/.config/kanata/kanata.kbd"
 exec sudo "$KANATA_BIN" --cfg "$KANATA_CFG"
