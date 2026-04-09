650d800d73e8a05d5887192f0e5c9844d2cb65d4
Novice <nvc@Penguin.local>
2026-04-09 21:09:55 +0530

    replace with blueutil

--- sketchybar/.config/sketchybar/plugins/bluetooth.sh
+++ sketchybar/.config/sketchybar/plugins/bluetooth.sh
@@ -1,10 +1,25 @@
-#!/bin/bash
-BT=$(system_profiler SPBluetoothDataType 2>/dev/null | grep "State:" | awk '{print $2}')
+# #!/bin/bash
+# BT=$(system_profiler SPBluetoothDataType 2>/dev/null | grep "State:" | awk '{print $2}')
+#
+# if [ "$BT" = "On" ]; then
+#   ICON="󰂯"
+# else
+#   ICON="󰂲"
+# fi
+#
+# sketchybar --set bluetooth icon="$ICON"
 
-if [ "$BT" = "On" ]; then
-  ICON="󰂯"
-else
+
+#!/bin/bash
+STATE=$(blueutil --power)
+CONNECTED=$(blueutil --connected | wc -l | tr -d ' ')
+
+if [ "$STATE" = "0" ]; then
   ICON="󰂲"
+elif [ "$CONNECTED" -gt "0" ]; then
+  ICON="󰂱"
+else
+  ICON="󰂯"
 fi
 
 sketchybar --set bluetooth icon="$ICON"
